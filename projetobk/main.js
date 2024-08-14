8de193877d762d6f93822f560169458bdfdc3321
Thiago Andrade <thiagoadd14@gmail.com>
2024-08-14 12:02:35 -0300

    adicionando o js e imagens

---
+++ projetobk/main.js
@@ -0,0 +1,12 @@
+var swiper = new Swiper(".swipper", {
+    cssMode: true,
+    loop: true,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+        el: ".swiper-pagination",
+    },
+    keyboard: true,
+});
